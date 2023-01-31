ec06e2dee4645c9539ac4c225de7118b576aaf1a
Facundo Abarca <facuabarca@gmail.com>
2023-01-31 21:21:35 +0100

    feat: adding core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ApiBridgeService } from './services/api-bridge.service';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [],
+  providers: [ApiBridgeService],
+})
+export class CoreModule {}
---
+++ src/app/core/services/api-bridge.service.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment as env } from '@environment/environment';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiBridgeService {
+  constructor(private httpClient: HttpClient) {}
+
+  get<T>(url: string, queryParams: any = null): Observable<T> {
+    let params = new HttpParams();
+    if (queryParams) {
+      params = Object.getOwnPropertyNames(queryParams).reduce(
+        (p, key) => p.set(key, queryParams[key]),
+        new HttpParams()
+      );
+    }
+    return this.httpClient.get<T>(env.api_url + url, { params });
+  }
+
+  post<T>(url: string, body: any = {}, headers?: any): Observable<T> {
+    return this.httpClient.post<T>(env.api_url + url, body, {
+      headers: new HttpHeaders({
+        ...headers,
+        'Content-Type': 'application/json; charset=utf-8',
+      }),
+    });
+  }
+}
