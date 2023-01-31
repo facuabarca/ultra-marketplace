d213471cc44b88e628e16886599f60703ab283b3
Facundo Abarca <facuabarca@gmail.com>
2023-01-31 21:23:51 +0100

    feat: implementing lazy-load and routes for the differents sections of the web app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasketModule } from './modules/basket/basket.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/listing/listing.module').then((m) => m.ListingModule),
+  },
+  {
+    path: 'basket',
+    loadChildren: () =>
+      import('./modules/basket/basket.module').then((m) => m.BasketModule),
+  },
+  {
+    path: 'checkout',
+    loadChildren: () =>
+      import('./modules/checkout/checkout.module').then(
+        (m) => m.CheckoutModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
