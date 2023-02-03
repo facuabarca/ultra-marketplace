5c6accfdb9f8257f9423c9eafbd7e2d91841331e
Facundo Abarca <facuabarca@gmail.com>
2023-02-03 21:50:20 +0100

    feat(electron): adding electron to project

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require("electron");
+
+let appWin;
+
+function isDev() {
+    return !app.isPackaged;
+}
+
+createWindow = () => {
+    appWin = new BrowserWindow({
+        width: 1400,
+        height: 1200,
+        title: "Ultra-Marketplace",
+        resizable: true,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true
+        }
+    });
+    
+    appWin.loadURL(`file://${__dirname}/dist/index.html`)
+
+    appWin.setMenu(null);
+
+    appWin.webContents.openDevTools();
+
+    appWin.on("closed", () => {
+        appWin = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+});
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'api-ultra/',
+  api_url: 'http://localhost:3000/',
 };
